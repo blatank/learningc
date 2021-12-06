762fb37bb6adead0087a6fa0eb65713fffcd81a8
blatank <paddocknote@gmail.com>
2021-12-07 08:30:49 +0900

    1-7まで実施

---
+++ 1-5.c
@@ -0,0 +1,11 @@
+/*
+    警報を2回発する
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("警報!!\a警報!!\a");
+
+    return 0;
+}
---
+++ 1-6-1.c
@@ -0,0 +1,14 @@
+/*
+    変数に整数値を代入して表示
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    x = 15;
+
+    printf("xの値は%dです。\n", x);
+
+    return 0;
+}
