c2b666dca9c53449379061509b87b0aac1052704
blatank <paddocknote@gmail.com>
2021-12-13 08:22:12 +0900

    2-7を追加

---
+++ chapter2/2-7.c
@@ -0,0 +1,17 @@
+/*
+    半径を読み込んで円の面積を求めて表示
+*/
+#include <stdio.h>
+#include <math.h>       /* M_PI */
+
+int main(void)
+{
+    double r;
+
+    printf("半径は：");
+    scanf("%lf", &r);
+
+    printf("円の面積は%lfです。\n", M_PI * r * r);    /* 円の面積 = π * rの2乗 */
+
+    return 0;
+}
