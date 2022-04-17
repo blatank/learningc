06209978cbc62da28542d6bd595fa20d40c0febf
blatank <paddocknote@gmail.com>
2022-04-17 18:36:55 +0900

    コメントの誤字修正と変数名見直し

--- chapter4/4-28.c
+++ chapter4/4-28.c
@@ -2,18 +2,18 @@
 
 int main(void)
 {
-    int h, i, j, stars, spaces, x;
+    int h, i, j, stars, spaces, half_line;
 
-    puts("段数を入力してください：");
+    printf("段数を入力してください：");
     scanf("%d", &h);
 
-    x = h - 1;
+    half_line = h - 1;
     for (i=0; i<h; i++) {
         /* 中心までに存在する*の数 stars = i */
         stars = i;
 
-        /* 中心までの空白数 = x - starts */
-        spaces = x - stars;
+        /* 中心までの空白数 = half_line - stars */
+        spaces = half_line - stars;
 
         /* 左側 */
         for (j=0; j<spaces; j++) {
