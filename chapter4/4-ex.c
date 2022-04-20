e9845a51f6cdab3a6229d7bb30b10457590e3476
blatank <paddocknote@gmail.com>
2022-04-20 22:32:35 +0900

    ソースが汚かったので修正

--- chapter4/4-ex.c
+++ chapter4/4-ex.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 
+/* 指定された数の*を出力して改行する */
+static void print_stars(int num);
+
+
 int main(void)
 {
-    int h, i, j, stars, lines;
+    int h, i, p;
 
-    printf("段数を入力してください：");
+    printf("右向きのピラミッドを作ります\n段数を入力してください：");
     scanf("%d", &h);
 
-    /* 実際の行数 */
-    lines = 2 * (h - 1) + 1;
-
-    for (i=0; i<h-1; i++) {
-        for (j=0; j<i+1; j++) {
-            putchar('*');
-        }
-        putchar('\n');
+    /* 真ん中までの段落の数(真ん中から最後までの数でもある) */
+    p = h - 1;
+    for (i=0; i<p; i++) {
+        /* その行で出力する*の数はi+1 */
+        print_stars(i+1);
     }
 
-    for (j=0; j<h; j++) {
-        putchar('*');
-    }
-    putchar('\n');
+    /* 真ん中の行はピラミッドの段数(=h) */
+    print_stars(h);
 
-    for (i=0; i<h-1; i++) {
-        stars = h - i - 1;
-        for (j=0; j<stars; j++) {
-            putchar('*');
-        }
-        putchar('\n');
+    /* 真ん中から下は真ん中から上までの逆になる */
+    /* その行で出力する*の数=h-i-1 */
+    for (i=0; i<p; i++) {
+        print_stars(h-i-1);
     }
 
     return 0;
+}
+
+/* 指定された数の*を出力して改行する */
+static void print_stars(int num)
+{
+    int i;
+    
+    for (i=0; i<num; i++) {
+        putchar('*');
+    }
+    printf("\n");
 }
